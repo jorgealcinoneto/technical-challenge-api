c7abaf476ef142c53be1fd918cb905740a049a4d
jorge.neto <jorgealcinoneto@gmail.com>
2019-11-03 17:42:47 -0300

    first commit

---
+++ src/test/java/com/jorgealcinoneto/technicalchallenge/api/TechnicalChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jorgealcinoneto.technicalchallenge.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
