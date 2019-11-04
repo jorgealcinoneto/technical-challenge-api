0c210a3c982ee2d7ffd9446c398ec4f21f657210
jorge.neto <jorgealcinoneto@gmail.com>
2019-11-04 08:06:01 -0300

    Add Spring JDBC Template Support

---
+++ src/main/java/com/jorgealcinoneto/technicalchallenge/api/config/JdbcConfig.java
@@ -0,0 +1,10 @@
+package com.jorgealcinoneto.technicalchallenge.api.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JdbcConfig {
+		
+	//TODO: Implements! Remenber Me!
+
+}
